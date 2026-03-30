4759227d1e26bf12f67369fd1ad484eec109c649
Michael Gene Brockus (Dreamer) <michaelbrockus@gmail.com>
2026-03-29 21:11:11 -0500

    manage headers in cases

--- code/tests/cases/test_socket.cpp
+++ code/tests/cases/test_socket.cpp
@@ -24,9 +24,16 @@
  */
 #include "fossil/network/framework.h"
 #include <fossil/pizza/framework.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
+
+#ifdef _WIN32
+    #include <winsock2.h>
+    #include <ws2tcpip.h>
+    #include <windows.h>
+#else
+    #include <sys/types.h>
+    #include <netinet/in.h>
+    #include <sys/socket.h>
+#endif
 
 
 // * * * * * * * * * * * * * * * * * * * * * * * *
